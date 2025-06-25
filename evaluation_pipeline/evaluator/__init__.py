0e75f39b8d4f4549fb6c5a49569c20cf3254ab10
Blanca Jimenez <blanca.jimenezvicente@gmail.com>
2025-06-25 12:45:22 +0200

    add accuracy evaluator

--- evaluation_pipeline/evaluator/__init__.py
+++ evaluation_pipeline/evaluator/__init__.py
@@ -1,3 +1,4 @@
-from .judge import LLMJudge
+from .format_evaluator import FormatEvaluator
 from .document_processor import ReportProcessor
 from .rewriter import ReportRewriter
+from .accuracy_evaluator import AccuracyEvaluator
