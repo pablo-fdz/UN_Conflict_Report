afe99f250779af4bbcca00cf3802338f47b8b9dc
pablo-fdz <pablo.fdzalb@gmail.com>
2025-07-07 11:50:03 +0200

    removed evaluation_pipeline folder (moved to limbo)

--- evaluation_pipeline/evaluator/__init__.py
+++
@@ -1,4 +0,0 @@
-from .format_evaluator import FormatEvaluator
-from .old_document_processor import ReportProcessor
-from .rewriter import ReportRewriter
-from .old_accuracy_evaluator import AccuracyEvaluator
